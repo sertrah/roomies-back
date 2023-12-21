71a83f74a39a73aca33876ca1a99f32c9096a5bf
Harlen Giraldo <harleng@createme.com>
2023-12-21 16:49:28 -0500

    create connection with mongo, add nestJS, create entities

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './module/user/user.module';
+import { ClientModule } from './module/client/client.module';
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING), //
+    UserModule,
+    ClientModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/module/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectConnection() private connection: Connection,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    // if no que viene de afuera
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
