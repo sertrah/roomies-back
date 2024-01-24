40b3ca7533fdd4732fa17f0bd98f3b24917ab580
Harlen Giraldo <harlen.giraldo@Harlen-Giraldo-MBP-M1-14.local>
2024-01-24 12:11:51 -0500

    add cors on main file

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,12 @@ import * as passport from "passport"
 
 
 async function bootstrap() {
-<<<<<<< Updated upstream
-  const app = await NestFactory.create(AppModule);
-<<<<<<< Updated upstream
-=======
-=======
   const app = await NestFactory.create(AppModule , {
     cors: {
       "origin": "*",
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "methods": "GET,HEAD,PUT,POST,DELETE",
     }
   });
->>>>>>> Stashed changes
 
   app.use(
     session({
@@ -30,12 +24,6 @@ async function bootstrap() {
   )
   app.use(passport.initialize())
   app.use(passport.session())
-<<<<<<< Updated upstream
-
-=======
-  app.enableCors()
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   await app.listen(3000);
 }
 bootstrap();
